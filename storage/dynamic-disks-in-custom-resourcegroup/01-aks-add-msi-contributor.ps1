6206ffcb167a100303aa8271e7a67ef615193d11
Richard Diphoorn <jodiphoo@microsoft.com>
2020-09-08 12:50:22 +0200

    Initial Commit

---
+++ storage/dynamic-disks-in-custom-resourcegroup/01-aks-add-msi-contributor.ps1
@@ -0,0 +1,14 @@
+﻿# When you want to deploy dynamic disks in another resource group, the AKS cluster needs to have contributor permissions on that
+# resource group. This code will assign the contributor role to the system MSI of the AKS cluster.
+
+$aksClusterName = 'testaks01' # Name of the cluster.
+$aksResourceGroupName = 'test-aks' # Name of the resource group in which the AKS cluster is deployed ( so NOT the MC_ rsg ).
+$storageResourceGroup = 'aksstorage' # Resource Group in which you want to deploy dynamic disks.
+
+$params = @{
+    RoleDefinitionName = 'Contributor'
+    ObjectId           = ( Get-AzAksCluster -Name $aksClusterName -ResourceGroupName $aksResourceGroupName).Identity.PrincipalId
+    Scope              = ( Get-AzResourceGroup -Name $storageResourceGroup ).ResourceId
+}
+
+New-AzRoleAssignment @params
